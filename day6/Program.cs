c763232637315685a67911bc612d1d5e3afd346d
ideefiix <flindu@gmail.com>
2022-12-06 11:25:54 +0100

    Day6

---
+++ day6/Program.cs
@@ -0,0 +1,8 @@
+﻿using day6;
+
+//Both puzzle 1,2 solutions are in the puzzle1 class
+//Puzzle 1 use 4 items in the list.
+//Puzzle 2 use 14 items in the list.
+
+Puzzle1 puzzle1 = new Puzzle1();
+puzzle1.PrintSolution();
---
+++ day6/Puzzle1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace day6;
+
+public class Puzzle1
+{
+    public void PrintSolution()
+    {
+        int newpacketMarker;
+        string communicationData =
+            System.IO.File.ReadAllText(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2022\day6\input.txt");
+        char[] charaters = communicationData.ToCharArray();
+        newpacketMarker = CharactersBeforeNewPacket(charaters);
+        Console.WriteLine("There are " + newpacketMarker + " characters before the start of a new packet");
+    }
+
+    public int CharactersBeforeNewPacket(char[] input)
+    {
+        int lastReadCharacter = 14;
+        List<char> currentCharacter = new List<char>();
+        currentCharacter.Add(input[0]);
+        currentCharacter.Add(input[1]);
+        currentCharacter.Add(input[2]);
+        currentCharacter.Add(input[3]);
+        currentCharacter.Add(input[4]);
+        currentCharacter.Add(input[5]);
+        currentCharacter.Add(input[6]);
+        currentCharacter.Add(input[7]);
+        currentCharacter.Add(input[8]);
+        currentCharacter.Add(input[9]);
+        currentCharacter.Add(input[10]);
+        currentCharacter.Add(input[11]);
+        currentCharacter.Add(input[12]);
+        currentCharacter.Add(input[13]);
+        if (ListHasUniqueItems(currentCharacter))
+        {
+            return lastReadCharacter;
+        }
+
+        for (int i = 14; i < input.Length; i++)
+        {
+            currentCharacter.RemoveAt(0);
+            currentCharacter.Add(input[i]);
+            lastReadCharacter++;
+            if (ListHasUniqueItems(currentCharacter))
+            {
+                return lastReadCharacter;
+            }
+        }
+
+        throw new Exception("Didn't find any start of packet marker!");
+
+    }
+
+    public bool ListHasUniqueItems(List<char> collection)
+    {
+        foreach (var item in collection)
+        {
+            if (collection.FindAll(c => c == item).Count >= 2)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
