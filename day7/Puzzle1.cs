05865c704611a4cfbea88206c98ad227173db8ad
ideefiix <flindu@gmail.com>
2022-12-08 22:09:38 +0100

    started on day7

---
+++ day7/Puzzle1.cs
@@ -0,0 +1,61 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace day7;
+
+public class Puzzle1
+{
+    public TreeNode root;
+    public TreeNode currentDirectory;
+
+    public Puzzle1()
+    {
+        root = new TreeNode(0, "/");
+        currentDirectory = root;
+    }
+
+    public void PrintPuzzle1Solution()
+    {
+        string[] lines =
+            System.IO.File.ReadAllLines(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2022\day7\input.txt");
+        lines[0] = "$ ls"; //This command is less annoying
+        
+        foreach (var line in lines)
+        {
+            
+        }
+        
+    }
+
+    public void InterpretLine(string line)
+    {
+        if (line[0] == '$') //Command
+        {
+            if(line.Substring(2,2) == "ls") return;
+
+            if (line.Substring(5,2) == "..")
+            {
+                currentDirectory = currentDirectory.Parent;
+            }
+            else
+            {
+                string directoryName = line.Substring(5);
+                TreeNode directory = currentDirectory.Children.Find(c => c.Name == directoryName); // Throws exception if none is found
+                currentDirectory = directory;
+
+            }
+        }
+        else //File
+        {
+            if (line.Substring(0,3) == "dir")
+            {
+                TreeNode child = new TreeNode(0, line.Substring(4));
+                currentDirectory.AddChild(child);
+            }
+            else
+            {
+                string[] valAndName = line.Split(' ');
+                TreeNode child = new TreeNode(Int32.Parse(valAndName[0]), valAndName[1]);
+            }
+        }
+    }
+}
---
+++ day7/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿namespace day7;
+
+public class TreeNode
+{
+    public TreeNode Parent { get; set; }
+    public List<TreeNode> Children { get; set; }
+    public int Value { get; set; } // Directories have value = 0
+    public string Name { get; set; } 
+
+    public TreeNode(int value, string name, TreeNode? parent = null)
+    {
+        Parent = parent;
+        Children = new List<TreeNode>();
+        Value = value;
+        Name = name;
+    }
+
+    public void AddChild(TreeNode child)
+    {
+        Children.Add(child);
+    }
+    
+}
